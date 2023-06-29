4255487b2981e51b381d2e1e5a966da23d3ac2b7
Perry Li <perrylee2008@gmail.com>
2023-06-28 21:17:22 -0400

    README update

--- Load more Spotify api info.py
+++ Load more Spotify api info.py
@@ -3,37 +3,32 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # Create sample data for the bar charts
-df = pd.read_json('./data/StreamingHistory0_NEW final - genre only.json')
+df = pd.read_json('./data/StreamingHistory0_NEW.json')
 
 # Connect to Spotify API
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 # Add new "genre" column
-# df['genre'] = [[] for _ in range(len(df))]
-# df['genre'] = ''
+df['genre'] = [[] for _ in range(len(df))]
 df['danceability'] = 0.0
 
 # Since Spotify API doesn't like too many request at once,
 # Slice df into 3 chunks and process one at a time
-n = 2700  #chunk row size
+n = 1000  #chunk row size
 list_df = [df[i:i+n] for i in range(0, len(df), n)]
-# list_df = [df[0:10], df[10:20], df[20:30]]
 
 # Request genre/danceability info from API through artistName
 for work_df in list_df:
     # Request genre info
-    # for index, row in work_df.iterrows():
-    #     artist_name = row['artistName']
-    #     results = spotify.search(q=artist_name, type='artist')
+    for index, row in work_df.iterrows():
+        artist_name = row['artistName']
+        results = spotify.search(q=artist_name, type='artist')
         
-    #     if results['artists']['items']:
-    #         if results['artists']['items'][0]['genres']:
-    #             genres = results['artists']['items'][0]['genres'][0]
-    #             work_df.at[index, 'genre'] = genres
-    #         else:
-    #             work_df.at[index, 'genre'] = 'Not found'
-    #     else:
-    #         work_df.at[index, 'genre'] = 'Not found'
+        if results['artists']['items']:
+            genres = results['artists']['items'][0]['genres']
+            work_df.at[index, 'genre'] = genres
+        else:
+            work_df.at[index, 'genre'] = ['Not found']
 
     # Request danceability info
     for index, row in work_df.iterrows():
@@ -50,49 +45,13 @@ for work_df in list_df:
 df_final = pd.concat(list_df)
 
 # Data cleaning for genre data
-# def fill_null(x):
-#     if len(x) == 0: 
-#         return 'Not found'
-#     else: 
-#         return x[0]
-# df['genre'] = df['genre'].apply(fill_null)
+def fill_null(x):
+    if len(x) == 0: 
+        return 'Not found'
+    else: 
+        return x[0]
+
+df_final['genre'] = df_final['genre'].apply(fill_null)
 
 # Export data to json file
 df_final.to_json('./data/StreamingHistory0_NEW final.json', orient='records')
-
-# Request danceability info from API through trackName
-# track_name = 'Cold As You'
-# results = spotify.search(track_name, type='track')
-# track_id = results['tracks']['items'][0]['id']
-
-# audio_features = spotify.audio_features(track_id)
-# test = audio_features[0]['danceability']
-
-
-# work_df = df[0:20]
-# list_df = [df[0:20], df[20:40], df[40:60]]
-
-# for work_df in list_df:
-#     for index, row in work_df.iterrows():
-#         track_name = row['trackName']
-#         results = spotify.search(track_name, type='track')
-        
-#         if results['tracks']['items']:
-#             track_id = results['tracks']['items'][0]['id']
-#             audio_features = spotify.audio_features(track_id)
-#             work_df.at[index, 'danceability'] = audio_features[0]['danceability']
-
-# Request genre info
-# for index, row in work_df.iterrows():
-#     artist_name = row['artistName']
-#     results = spotify.search(q=artist_name, type='artist')
-    
-#     if results['artists']['items'][0]['genres']:
-#         genres = results['artists']['items'][0]['genres'][0]
-#         work_df.at[index, 'genre'] = genres
-#     else:
-#         work_df.at[index, 'genre'] = 'Not found'
-
-# artist_name = 'The Joe Rogan Experience'
-# results = spotify.search(artist_name, type='artist')
-# genres = results['artists']['items'][0]['genres'][0]
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# spotify_wrapped
+# **Spotify Wrapped for 2022**
+## Background
+Every year, Spotify sends a personalized report to each user called “Spotify Wrapped.” It’s called “Wrapped” since it’s a little holiday gift to you. It’s a fun, well-designed summary of what the user listens to throughout the year. It’s really fun & interesting which leads to people sharing with family & friends.  
+This is great marketing for Spotify. Thousands of people saying the word “Spotify” & honestly, those that don’t have Spotify feel a little left out. Hence, this analytics report is worth a lot of Spotify!  
+[Here's an example on youtube.](https://youtu.be/KknMSXE3a-c?t=58).  
+## Project scope
+This project is to create my own version of Spotify Wrapped.  
+The project is implemented in several steps:  
+1. [Request a download dump from Spotify.](https://www.spotify.com/us/account/privacy/) It takes a few days to get dump from Spotify in json format.
+2. Create an interactive dashboard / web application using Streamlit in Python.
+3. Link streamlit app with [Spotify API](https://developer.spotify.com/documentation/web-api) data to obtain genres and audio features endpoint using [Python parckage Spotipy](https://spotipy.readthedocs.io/en/2.22.1/).  
+## Streamlit Web app
+[2022 Spotify Wrapped for Perry report can be accessed through this link.](https://spotify-wrapped.streamlit.app/)
+Web page consists of 4 sections:
+### Section 1: Show Top Artist & Top Song user listened most during 2022.
+### Section 2: Top 10 Artists and top 15 songs listened most were shown in bar charts.
+### Section 3: Two interactive charts.  
+First one is radar chart, displays how many time user listened to Spotify during time of day.  
+If you hover over any point of brown line, exact time and how many times listened will be displayed.  
+Another histogram shows distribution of length of time spend on Spotify. Again, hover over will display details.
+### Section 4: Top Genres listened and Songs with most Danceability in bar charts
+Through Spotify API calls, Genres info and Danceability audio features were obtained and added back to json file. Seperate python program was used since it takes quite long time to complete thousands of api queries. 
