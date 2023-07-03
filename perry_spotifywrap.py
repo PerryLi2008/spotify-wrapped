e92f3ede74d6a309a6cf17f9bb3a2c7635a63f82
Perry Li <perrylee2008@gmail.com>
2023-07-03 00:57:20 -0400

    Add colors

--- perry_spotifywrap.py
+++ perry_spotifywrap.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
@@ -7,11 +8,19 @@ from urllib.request import urlopen
 from PIL import Image
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from datetime import timedelta, datetime
 
 st.set_page_config(layout="wide")
 
 # Set the title of the app
-st.title("2022 Spotify Wrapped for Perry")
+st.title("Spotify Wrapped for Perry")
+st.write("October 2022 - June 2023")
+# color_purple = st.color_picker('purple', '#6806bf', label_visibility='hidden')
+# color_orange = st.color_picker('orange', '#f78b00')
+# color_yellow = st.color_picker('yellow', '#f2ff2b')
+# color_pink = st.color_picker('pink', '#ee74c5')
+
+# st.header(""":pink[ Spotify ] :orange[Wrapped] :yellow[for] :purple[Perry]""")
 
 # Create sample data for the bar charts
 df = pd.read_json('./data/StreamingHistory0 final.json')
@@ -33,16 +42,17 @@ num_genres_listened = num_genres_listened.reset_index(names='Genre').iloc[1:11]
 
 # Get Most Dancibility list
 unique_trackName = df['trackName'].unique()
-most_danceability = pd.DataFrame(unique_trackName, columns=['trackName'])
-most_danceability['danceability'] = 0.0
+danceability_scores = pd.DataFrame(unique_trackName, columns=['trackName'])
+danceability_scores['danceability'] = 0.0
 
-for index, row in most_danceability.iterrows():
+for index, row in danceability_scores.iterrows():
     track_name = row['trackName']
     danceability_num = df['danceability'][df['trackName'] == track_name].values[0]
-    most_danceability.at[index, 'danceability'] = danceability_num
+    danceability_scores.at[index, 'danceability'] = danceability_num
 
-# Get hour
-df['datetime'] = pd.to_datetime(df['endTime'])
+# Get hour. Since Spotify uses UTC time to generate dump file
+# There're 4 hours difference between UTC and EDT, has to convert for hour
+df['datetime'] = pd.to_datetime(df['endTime']).apply(lambda dt: dt - timedelta(hours=4))
 df['Hour'] = df['datetime'].dt.hour
 
 # Plot Top artist and Top Song
@@ -90,15 +100,38 @@ col3, col4  = st.columns(2)
 # Create the first bar chart
 with col3: 
     st.subheader("Top Artists")
-    num_artists_listened = num_artists_listened.sort_values(by='count')
+    num_artists_listened = num_artists_listened.sort_values('count')
 
-    fig1, ax1 = plt.subplots()
-    ax1.barh(num_artists_listened['Artist'], num_artists_listened['count'])
-    plt.xlabel('# of Times Played')
-    plt.ylabel('Artists')
-    plt.title('Top Artists')
-    st.pyplot(fig1)
+    # fig1, ax1 = plt.subplots()
+    # ax1.barh(num_artists_listened['Artist'], num_artists_listened['count'])
+    # plt.xlabel('# of Times Played')
+    # plt.ylabel('Artists')
+    # plt.title('Top Artists')
+    # st.pyplot(fig1)
 
+    # Create a color gradient based on the size of x-axis values
+    colorscale = [[0, '#8b08fd'], [1, '#6806bd']]
+    hover_template = "User played %{y}'s songs %{x} times"
+
+    fig_bar1 = go.Figure()    
+    fig_bar1.add_trace(go.Bar(
+        x=num_artists_listened['count'],
+        y=num_artists_listened['Artist'],
+        orientation='h',
+        marker=dict(
+        color=num_artists_listened['count'],
+        colorscale=colorscale,
+        showscale=False
+        ),
+        hovertemplate=hover_template
+    ))
+
+    fig_bar1.update_layout(
+        xaxis_title='# of Times Played',
+        yaxis_title='Artists'
+    )
+
+    st.plotly_chart(fig_bar1, use_container_width=True)
 
 # Create the second bar chart
 with col4: 
@@ -106,12 +139,36 @@ with col4:
     num_songs_listened = num_songs_listened.sort_values(by='count')
     num_songs_listened['Track'] = num_songs_listened['Track'].str.slice(0,30)
 
-    fig2, ax2 = plt.subplots()
-    ax2.barh(num_songs_listened['Track'], num_songs_listened['count'])
-    plt.xlabel('# of Times Played')
-    plt.ylabel('Songs')
-    plt.title('Top Songs')
-    st.pyplot(fig2)
+    # fig2, ax2 = plt.subplots()
+    # ax2.barh(num_songs_listened['Track'], num_songs_listened['count'])
+    # plt.xlabel('# of Times Played')
+    # plt.ylabel('Songs')
+    # plt.title('Top Songs')
+    # st.pyplot(fig2)
+
+    # Create a color gradient based on the size of x-axis values
+    colorscale = [[0, '#fc9b39'], [1, '#bc732b']]
+    hover_template = "User played %{y} %{x} times"
+
+    fig_bar2 = go.Figure()    
+    fig_bar2.add_trace(go.Bar(
+        x=num_songs_listened['count'],
+        y=num_songs_listened['Track'],
+        orientation='h',
+        marker=dict(
+        color=num_songs_listened['count'],
+        colorscale=colorscale,
+        showscale=False
+        ),
+        hovertemplate=hover_template
+    ))
+
+    fig_bar2.update_layout(
+        xaxis_title='# of Times Played',
+        yaxis_title='Songs'
+    )
+
+    st.plotly_chart(fig_bar2, use_container_width=True)
 
 # Plot Listening by Time of Day and Track Length Distribution
 col5, col6  = st.columns(2)
@@ -136,6 +193,8 @@ with col5:
         return hr_str + suffix
 
     thetas = [hr2angle(i) for i in num_listens_by_hour['Hour']]
+    hover_template = "Play songs %{r} times at %{theta}"
+    
     data = [
         go.Scatterpolar(
             r = num_listens_by_hour['count'],
@@ -143,7 +202,8 @@ with col5:
             mode = 'lines',
             marker = dict(
                 color = 'peru'
-            )
+            ),
+            hovertemplate=hover_template
         )
     ]
 
@@ -170,7 +230,29 @@ with col6:
     # df['podcast'] = 0
     # df['podcast'][df['minutesPlayed'] > 6] = 1
 
-    fig_histogram = px.histogram(df, x='minutesPlayed')
+    # fig_histogram = px.histogram(df, x='minutesPlayed')
+    colorscale = [[0, '#54ff68'], [1, '#36a342']]
+    hover_template = "Songs with %{x} minutes length were played %{y} times"
+    # Compute the total counts for each bin
+    counts, bins = np.histogram(df['minutesPlayed'], bins=np.arange(0.0, 20.0, 0.1))
+
+    fig_histogram = go.Figure()
+    fig_histogram.add_trace(go.Bar(
+        x=bins,
+        y=counts,
+        marker=dict(
+        color=counts,
+        colorscale=colorscale,
+        showscale=False
+        ),
+        hovertemplate=hover_template
+    ))
+
+    fig_histogram.update_layout(
+        xaxis_title='Track Minutes Played',
+        yaxis_title='Counts'
+    )
+
     st.plotly_chart(fig_histogram)
 
 # Plot Top Genres and Most Danceability
@@ -181,21 +263,73 @@ with col7:
     st.subheader("Top Genres")
     num_genres_listened = num_genres_listened.sort_values(by='count')
 
-    fig3, ax3 = plt.subplots()
-    ax3.barh(num_genres_listened['Genre'], num_genres_listened['count'])
-    plt.xlabel('# of Times Played')
-    plt.ylabel('Genres')
-    st.pyplot(fig3)
+    # fig3, ax3 = plt.subplots()
+    # ax3.barh(num_genres_listened['Genre'], num_genres_listened['count'])
+    # plt.xlabel('# of Times Played')
+    # plt.ylabel('Genres')
+    # st.pyplot(fig3)
+
+        # Create a color gradient based on the size of x-axis values
+    colorscale = [[0, '#fe7bd2'], [1, '#c35ea1']]
+    hover_template = "User played %{y} songs %{x} times"
+
+    fig_bar3 = go.Figure()    
+    fig_bar3.add_trace(go.Bar(
+        x=num_genres_listened['count'],
+        y=num_genres_listened['Genre'],
+        orientation='h',
+        marker=dict(
+        color=num_genres_listened['count'],
+        colorscale=colorscale,
+        showscale=False
+        ),
+        hovertemplate=hover_template
+    ))
+
+    fig_bar3.update_layout(
+        xaxis_title='# of Times Played',
+        yaxis_title='Genres'
+    )
+
+    st.plotly_chart(fig_bar3, use_container_width=True)
 
 # # Create Most Danceability list
 with col8:
     st.subheader("Songs with Most Danceability")
-    most_danceability = most_danceability.sort_values(by='danceability', ascending=False)
-    most_danceability = most_danceability.head(10).reset_index(drop=True)
+    danceability_scores = danceability_scores.sort_values('danceability', ascending=False).head(15)
+    # danceability_scores = danceability_scores.reset_index(drop=True)
     
-    fig4, ax4 = plt.subplots()
-    ax4.barh(most_danceability['trackName'], most_danceability['danceability'])
-    ax4.invert_yaxis()
-    plt.xlabel('Danceability score')
-    plt.ylabel('Songs')
-    st.pyplot(fig4, use_container_width=True)
+    # blank_row = pd.DataFrame({}, columns=most_danceability.columns)
+    # most_danceability.loc[len(most_danceability)] = ['', np.nan]
+    # danceability_scores = pd.concat([most_danceability, least_danceability], ignore_index=True)
+
+    # fig4, ax4 = plt.subplots()
+    # ax4.barh(most_danceability['trackName'], most_danceability['danceability'])
+    # ax4.invert_yaxis()
+    # plt.xlabel('Danceability score')
+    # plt.ylabel('Songs')
+    # st.pyplot(fig4, use_container_width=True)
+
+    colorscale = [[0, '#f2ff2e'], [1, '#dee82a']]
+    hover_template = "%{y}'s danceability score is %{x}"
+
+    fig_bar4 = go.Figure()    
+    fig_bar4.add_trace(go.Bar(
+        x=danceability_scores['danceability'],
+        y=danceability_scores['trackName'],
+        orientation='h',
+        marker=dict(
+        color=danceability_scores['danceability'],
+        colorscale=colorscale,
+        showscale=False
+        ),
+        hovertemplate=hover_template
+    ))
+
+    fig_bar4.update_layout(
+        yaxis=dict(autorange="reversed"),
+        xaxis_title='Danceability score',
+        yaxis_title='Songs'
+    )
+
+    st.plotly_chart(fig_bar4, use_container_width=True)
